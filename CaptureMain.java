09c3d6a5a52009a772c2cf2d3d2a09559ba5dc91
BrennanG <brennangovreau@yahoo.com>
2014-11-19 00:18:48 -0600

    Update CaptureMain.java

--- CaptureMain.java
+++ CaptureMain.java
@@ -1,5 +1,16 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
 public class CaptureMain {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
         
+        GameManager game = new GameManager();
+        
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
